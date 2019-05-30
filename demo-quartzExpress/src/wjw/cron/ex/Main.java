568d51ce47ba40637359fbb0768d06bc47db23b3
dhstime <542068772@qq.com>
2019-05-30 15:40:47 +0800

    main修改

--- demo-quartzExpress/src/wjw/cron/ex/Main.java
+++ demo-quartzExpress/src/wjw/cron/ex/Main.java
@@ -20,6 +20,7 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String args[]) {
+	  String a;
     initCustomLAF();
     java.awt.EventQueue.invokeLater(new Runnable() {
 
